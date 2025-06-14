c0971484ab407ec14e50e5aa3b852cfddced17d7
DanielBMoura <dborazo.moura@gmail.com>
2025-06-14 00:48:14 -0300

    Ultima atualização blogApp

--- helpers/eAdmin.js
+++ helpers/eAdmin.js
@@ -3,10 +3,9 @@
 
 module.exports = {
     eAdmin: function(req, res, next) {
-        if (1 == 1) {    // Função que verifica se certo usuario está autenticado ou não E se ele tem o eadmin igual a 1
+        if (req.isAuthenticated() && req.user.eAdmin == 1) {    // Função que verifica se certo usuario está autenticado ou não E se ele tem o eadmin igual a 1
             return next()   // <- Se tive, ele pode acessar normalmente a rota
         }
-        // req.isAuthenticated() && req.user.eAdmin == 1
         req.flash('error_msg', 'Você precisa ser Admin para acessar essa rota') // Se não, isso é executado
         res.redirect('/')
     }
